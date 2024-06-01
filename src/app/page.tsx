58dee65467be647dfbf7f7a8bae9dba9f0a3e049
unknown <harrison.bowmar@mcglashan.school.nz>
2024-06-01 14:55:25 +1200

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button className="bg-green-100" variant="outline">Button</Button>
+
+      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        hehe
+        <code className="font-mono font-bold">fronk</code>
+      </p>
+
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </main>
+  );
+}
